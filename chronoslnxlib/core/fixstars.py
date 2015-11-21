fc2a97cf5e28f805bc4c77b822a02fe2fa347978
ShadowKyogre <shadowkyogre.public@gmail.com>
2015-11-20 19:23:15 -0800

    Avg fix stars func, fix with nearest_fixed_star
    Elaborating on the second, I forgot to set the second thing to infinity
    if the star position to the right. I also accidentally swapped the diff
    calculations.

--- chronoslnxlib/core/fixstars.py
+++ chronoslnxlib/core/fixstars.py
@@ -2,7 +2,7 @@ import swisseph
 
 from bisect import bisect_left, bisect_right
 
-from . import datetime_to_julian, angle_sub
+from . import datetime_to_julian, angle_sub, zipped_func
 
 # Default list of fixed stars
 WANTED_STARS = [
@@ -59,6 +59,28 @@ WANTED_STARS = [
 	'Scheat',
 ]
 
+def average_verify_fixed_star(fs1, fs2):
+	if fs1[0] != fs2[0]:
+		raise ValueError(("Can't average positions for different fixed stars: "
+	                      "{0} != {1}".format(fs1[0], fs2[0])))
+	else:
+		return fs1[0], average(fs1[1], fs2[1])
+
+def average_fixed_stars(fixed_stars_1, fixed_stars_2):
+	fixed_stars_copy1 = fixed_stars_1[:]
+	fixed_stars_copy2 = fixed_stars_2[:]
+
+	fixed_stars_copy1.sort(key=lambda x: x[0])
+	fixed_stars_copy2.sort(key=lambda x: x[0])
+	# line the copies up by star name
+
+	output = zipped_func(fixed_stars_copy1, fixed_stars_copy2, 
+	                     func=average_verify_fixed_star)
+
+	output.sort(key=lambda x: x[1])
+	output_keys = [x[1] for x in output]
+	return output_keys, output
+
 def list_fixed_stars(date, wanted_stars=WANTED_STARS):
 	output = []
 
@@ -74,16 +96,20 @@ def list_fixed_stars(date, wanted_stars=WANTED_STARS):
 
 def nearest_fixed_star(positions, wanted_star_keys, wanted_stars,  orb=3):
 	output = {}
+	lenkeys = len(wanted_star_keys)
 
 	for pos in positions:
 		closest_staridx_1 = bisect_left(wanted_star_keys, pos)
 		closest_staridx_2 = bisect_right(wanted_star_keys, pos)
 
 		closest_starpos_1 = wanted_star_keys[closest_staridx_1]
-		closest_starpos_2 = wanted_star_keys[closest_staridx_2]
+		if closest_staridx_2 >= lenkeys:
+			closest_starpos_2 = float('inf')
+		else:
+			closest_starpos_2 = wanted_star_keys[closest_staridx_2]
 
-		dist1 = abs(angle_sub(pos, closest_starpos_2))
-		dist2 = abs(angle_sub(pos, closest_starpos_1))
+		dist1 = abs(angle_sub(pos, closest_starpos_1))
+		dist2 = abs(angle_sub(pos, closest_starpos_2))
 
 		in_orb1 = dist1 <= orb
 		in_orb2 = dist2 <= orb
