0b87a17b002e960212f38d824c229284f3ee4e30
ShadowKyogre <shadowkyogre.public@gmail.com>
2016-07-22 18:35:26 -0700

    Fully publicize day fill/fg fields

--- chronoslnxlib/csscalendar.py
+++ chronoslnxlib/csscalendar.py
@@ -26,11 +26,11 @@ class TodayDelegate(QtWidgets.QStyledItemDelegate):
             painter.restore()
 
 class CSSCalendar(QtWidgets.QWidget):
-    currentPageChanged = QtCore.pyqtSignal(int,int)
+    currentPageChanged = QtCore.pyqtSignal(int, int)
     """A CalendarWidget that supports CSS theming"""
-    def __init__(self, *args,**kwargs):
+    def __init__(self, *args, **kwargs):
         super().__init__(*args)
-        self.__useCSS = False
+        self._useCSS = False
         layout = QtWidgets.QGridLayout(self)
         layout.setSpacing(0)
         self._monthBox = QtWidgets.QComboBox(self)
@@ -38,7 +38,7 @@ class CSSCalendar(QtWidgets.QWidget):
         self._goForward = QtWidgets.QToolButton()
         self._goBackward = QtWidgets.QToolButton()
         self._yearBox = QtWidgets.QLineEdit(self)
-        self._table = QtWidgets.QTableWidget(6,7)
+        self._table = QtWidgets.QTableWidget(6, 7)
 
         self._table.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
         self._table.setHorizontalHeaderLabels(calendar.day_abbr[6:]+calendar.day_abbr[:6])
@@ -56,11 +56,11 @@ class CSSCalendar(QtWidgets.QWidget):
         self._goForward.setArrowType(QtCore.Qt.RightArrow)
         self._monthBox.activated[int].connect(self.setMonth)
 
-        layout.addWidget(self._goBackward,0,0)
-        layout.addWidget(self._monthBox,0,1,1,3)
-        layout.addWidget(self._yearBox,0,4,1,1)
-        layout.addWidget(self._goForward,0,5)
-        layout.addWidget(self._table,1,0,4,6)
+        layout.addWidget(self._goBackward, 0, 0)
+        layout.addWidget(self._monthBox, 0, 1, 1, 3)
+        layout.addWidget(self._yearBox, 0, 4, 1, 1)
+        layout.addWidget(self._goForward, 0, 5)
+        layout.addWidget(self._table, 1, 0, 4, 6)
 
         self.weekdayBGs = [QtGui.QBrush() for i in range(7)]
         self.weekdayFGs = [QtGui.QBrush() for i in range(7)]
@@ -86,21 +86,21 @@ class CSSCalendar(QtWidgets.QWidget):
     def prevPage(self):
         if self._date.month == 1:
             #self.currentPageChanged.emit(self._date.year-1,12)
-            self.setCurrentPage(self._date.year-1,12)
+            self.setCurrentPage(self._date.year-1, 12)
         else:
             #self.currentPageChanged.emit(self._date.year,self._date.month-1)
-            self.setCurrentPage(self._date.year,self._date.month-1)
+            self.setCurrentPage(self._date.year, self._date.month-1)
 
     def nextPage(self):
         if self._date.month == 12:
             #self.currentPageChanged.emit(self._date.year+1,11)
-            self.setCurrentPage(self._date.year+1,1)
+            self.setCurrentPage(self._date.year+1, 1)
         else:
             #self.currentPageChanged.emit(self._date.year,self._date.month+1)
-            self.setCurrentPage(self._date.year,self._date.month+1)
+            self.setCurrentPage(self._date.year, self._date.month+1)
 
     def setMonth(self, monthidx):
-        self.currentPageChanged.emit(self._date.year,monthidx+1)
+        self.currentPageChanged.emit(self._date.year, monthidx+1)
         try:
             self.date = self.date.replace(month=monthidx+1)
         except ValueError as e:
@@ -159,24 +159,24 @@ class CSSCalendar(QtWidgets.QWidget):
             item.setData(QtCore.Qt.UserRole, monthdates[i])
             item.setData(QtCore.Qt.TextAlignmentRole, QtCore.Qt.AlignCenter)
             if self.useCSS:
-                item.setForeground(self.weekdayFGs[i%7])
-                item.setBackground(self.weekdayBGs[i%7])
+                item.setForeground(self.weekdayFGs[i % 7])
+                item.setBackground(self.weekdayBGs[i % 7])
             if monthdates[i] == self._date:
-                idxs = (i/7,i%7)
+                idxs = (i / 7, i % 7)
             self._modifyDayItem(item)
             if item.data(QtCore.Qt.UserRole+1):
                 self._todayItem = item
-            self._table.setItem(i/7,i%7,item)
+            self._table.setItem(i / 7, i % 7, item)
         self._table.setCurrentCell(*idxs)
         self._table.resizeColumnsToContents()
         self._table.resizeRowsToContents()
 
     def useCSS(self):
-        return self.__useCSS
+        return self._useCSS
 
     def setUseCSS(self, css):
-        self.__useCSS = css
-        if not self.__useCSS:
+        self._useCSS = css
+        if not self._useCSS:
             transparent = QtGui.QColor()
             transparent.setAlpha(0)
             brush = QtGui.QBrush(transparent)
@@ -197,89 +197,89 @@ class CSSCalendar(QtWidgets.QWidget):
     def setDayFG(self, day, fg):
         self.weekdayFGs[day-1] = fg
         for i in range(self._table.rowCount()):
-            self._table.item(i,day-1).setForeground(fg)
+            self._table.item(i, day-1).setForeground(fg)
 
     def setDayFill(self, day, fill):
         self.weekdayBGs[day-1] = fill
         for i in range(self._table.rowCount()):
-            self._table.item(i,day-1).setBackground(fill)
+            self._table.item(i, day-1).setBackground(fill)
 
-    def _dayFG(self, day):
+    def dayFG(self, day):
         return self.weekdayFGs[day-1]
 
-    def _dayFill(self, day):
+    def dayFill(self, day):
         return self.weekdayBGs[day-1]
 
     sundayFill = QtCore.pyqtProperty(
         "QBrush",
-        lambda self: self._dayFill(QtCore.Qt.Sunday),
-        lambda self, fill: self._setDayFill(QtCore.Qt.Sunday, fill)
+        lambda self: self.dayFill(QtCore.Qt.Sunday),
+        lambda self, fill: self.setDayFill(QtCore.Qt.Sunday, fill)
     )
     mondayFill = QtCore.pyqtProperty(
         "QBrush",
-        lambda self: self._dayFill(QtCore.Qt.Monday), 
-        lambda self, fill: self._setDayFill(QtCore.Qt.Monday,fill)
+        lambda self: self.dayFill(QtCore.Qt.Monday), 
+        lambda self, fill: self.setDayFill(QtCore.Qt.Monday, fill)
     )
     tuesdayFill = QtCore.pyqtProperty(
         "QBrush",
-        lambda self: self._dayFill(QtCore.Qt.Tuesday), 
-        lambda self,fill: self._setDayFill(QtCore.Qt.Tuesday,fill)
+        lambda self: self.dayFill(QtCore.Qt.Tuesday), 
+        lambda self, fill: self.setDayFill(QtCore.Qt.Tuesday, fill)
     )
     wednesdayFill = QtCore.pyqtProperty(
         "QBrush",
-        lambda self: self._dayFill(QtCore.Qt.Wednesday), 
-        lambda self,fill: self._setDayFill(QtCore.Qt.Wednesday,fill)
+        lambda self: self.dayFill(QtCore.Qt.Wednesday), 
+        lambda self, fill: self.setDayFill(QtCore.Qt.Wednesday, fill)
     )
     thursdayFill = QtCore.pyqtProperty(
         "QBrush",
-        lambda self: self._dayFill(QtCore.Qt.Thursday), 
-        lambda self,fill: self._setDayFill(QtCore.Qt.Thursday,fill)
+        lambda self: self.dayFill(QtCore.Qt.Thursday), 
+        lambda self, fill: self.setDayFill(QtCore.Qt.Thursday, fill)
     )
     fridayFill = QtCore.pyqtProperty(
         "QBrush",
-        lambda self: self._dayFill(QtCore.Qt.Friday), 
-        lambda self,fill: self._setDayFill(QtCore.Qt.Friday,fill)
+        lambda self: self.dayFill(QtCore.Qt.Friday), 
+        lambda self, fill: self.setDayFill(QtCore.Qt.Friday, fill)
     )
     saturdayFill = QtCore.pyqtProperty(
         "QBrush",
-        lambda self: self._dayFill(QtCore.Qt.Saturday), 
-        lambda self,fill: self._setDayFill(QtCore.Qt.Saturday,fill)
+        lambda self: self.dayFill(QtCore.Qt.Saturday), 
+        lambda self, fill: self.setDayFill(QtCore.Qt.Saturday, fill)
     )
 
     sundayFG = QtCore.pyqtProperty(
         "QBrush",
-        lambda self: self._dayFG(QtCore.Qt.Sunday), 
-        lambda self,fill: self._setDayFG(QtCore.Qt.Sunday,fill)
+        lambda self: self.dayFG(QtCore.Qt.Sunday), 
+        lambda self, fill: self.setDayFG(QtCore.Qt.Sunday, fill)
     )
     mondayFG = QtCore.pyqtProperty(
         "QBrush",
-        lambda self: self.__dayFG(QtCore.Qt.Monday), 
-        lambda self,fill: self._setDayFG(QtCore.Qt.Monday,fill)
+        lambda self: self.dayFG(QtCore.Qt.Monday), 
+        lambda self, fill: self.setDayFG(QtCore.Qt.Monday, fill)
     )
     tuesdayFG = QtCore.pyqtProperty(
         "QBrush",
-        lambda self: self._dayFG(QtCore.Qt.Tuesday), 
-        lambda self,fill: self._setDayFG(QtCore.Qt.Tuesday,fill)
+        lambda self: self.dayFG(QtCore.Qt.Tuesday), 
+        lambda self, fill: self.setDayFG(QtCore.Qt.Tuesday, fill)
     )
     wednesdayFG = QtCore.pyqtProperty(
         "QBrush",
-        lambda self: self._dayFG(QtCore.Qt.Wednesday), 
-        lambda self,fill: self._setDayFG(QtCore.Qt.Wednesday,fill)
+        lambda self: self.dayFG(QtCore.Qt.Wednesday), 
+        lambda self, fill: self.setDayFG(QtCore.Qt.Wednesday, fill)
     )
     thursdayFG = QtCore.pyqtProperty(
         "QBrush",
-        lambda self: self._dayFG(QtCore.Qt.Thursday), 
-        lambda self,fill: self._setDayFG(QtCore.Qt.Thursday,fill)
+        lambda self: self.dayFG(QtCore.Qt.Thursday), 
+        lambda self, fill: self.setDayFG(QtCore.Qt.Thursday, fill)
     )
     fridayFG = QtCore.pyqtProperty(
         "QBrush",
-        lambda self: self._dayFG(QtCore.Qt.Friday), 
-        lambda self,fill: self._setDayFG(QtCore.Qt.Friday,fill)
+        lambda self: self.dayFG(QtCore.Qt.Friday), 
+        lambda self, fill: self.setDayFG(QtCore.Qt.Friday, fill)
     )
     saturdayFG = QtCore.pyqtProperty(
         "QBrush",
-        lambda self: self._dayFG(QtCore.Qt.Saturday), 
-        lambda self,fill: self._setDayFG(QtCore.Qt.Saturday,fill)
+        lambda self: self.dayFG(QtCore.Qt.Saturday), 
+        lambda self, fill: self.setDayFG(QtCore.Qt.Saturday, fill)
     )
     '''
     weekNFG=QtCore.pyqtProperty("QBrush", _weekNFG, _setWeekNFG)
